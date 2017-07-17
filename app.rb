76b74fcdd3fd415336d4985de096711efee9c082
Benjamin-Coleman <github email address>
2017-07-17 13:31:05 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+	get('/name') {"My name is Ben"}
+
+	get('/hometown') {"My hometown is Glastonbury"}
+
+	get('/favorite-song') {"My favorite song is Gone by Kanye West"}
 end
+
+
+# The name route should display "My name is __" in the browser, 
+# the hometown route should display "My hometown is __", and the favorite-song route should display "My favorite song is __".
